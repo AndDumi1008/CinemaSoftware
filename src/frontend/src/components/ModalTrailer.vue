695caa698fa7a2afaa11b1a0dd7ce9f5e759e96e
Andrei Dumitrescu <andrei.dumitrescu100801@gmail.com>
2022-05-17 23:21:26 +0300

    added trailer modal

---
+++ src/frontend/src/components/ModalTrailer.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+  name: 'ModalTrailer',
+  props: {
+    show: Boolean,
+    url: String,
+  },
+
+}
+</script>
+
+<template>
+  <Transition name="modal-trailer">
+    <div v-if="show" class="modal-mask" >
+    <div class="modal-wrapper" @click="$emit('close')">
+      <div class="video-container">
+          <iframe src="https://www.youtube.com/embed/r7OAo58GZc0?autoplay=1&mute=1" class="video-player"/>
+    </div>
+    </div>
+    </div>
+
+  </Transition>
+</template>
+
+<style>
+
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.video-container {
+  position: relative;
+  padding-bottom: 56.25%;
+  height: 0;
+}
+
+.video-player{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 55%;
+  height: 50%;
+
+
+  display: block;
+  margin-left: auto;
+  margin-right: auto
+}
+
+
+</style>
