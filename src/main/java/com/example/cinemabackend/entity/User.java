89ab63ebdb4522f2b92b56fcdcdde9214d161ac0
adrian <adrianivan0010@gmail.com>
2022-05-17 12:22:05 +0300

    Made user class

--- src/main/java/com/example/cinemabackend/entity/User.java
+++ src/main/java/com/example/cinemabackend/entity/User.java
@@ -1,4 +1,14 @@
 package com.example.cinemabackend.entity;
 
+import com.example.cinemabackend.service.TicketImpl;
+import lombok.Builder;
+import lombok.Data;
+import org.bson.types.ObjectId;
+
+@Data
+@Builder
 public class User {
+
+    private ObjectId movieId;
+    private TicketImpl tickets;
 }
